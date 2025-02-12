2d3c2e74550ac8028cbd77e7d8e29d4ab1a6acfd
simplyObare <josephatowiny@gmail.com>
2025-02-12 05:50:49 +0300

    Create urls.py and Configure URLs (Products App)

--- django-ecommerce/products/views.py
+++ django-ecommerce/products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
 
 # Create your views here.
+def product_list(request, category_slug):
+    category = None
+    products = Product.objects.filter(available=True)
+    categories = Category.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(
+        request,
+        "products/product/products_list.html",
+        {"category": category, "products": products, "categories": categories},
+    )
+
+
+def product_detail(request, pk, slug):
+    product = get_object_or_404(Product, pk=pk, slug=slug, available=True)
+    return render(
+        request,
+        "products/product/product_detail.html",
+        {"product": product},
+    )
