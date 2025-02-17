7b4e1164136a47693aea8d5e071b1e9df9683a01
simplyObare <josephatowiny@gmail.com>
2025-02-17 10:32:13 +0300

    Implementing a Custom User Model

--- blog-app/blog_api/admin.py
+++ blog-app/blog_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- blog-app/blog_api/models.py
+++ blog-app/blog_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures", null=True, blank=True
+    )
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    youtube = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name_plural = "Users"
