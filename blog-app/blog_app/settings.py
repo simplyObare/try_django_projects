5b98aa71869f21ba5ae0743fbfb8e184c01863f9
simplyObare <josephatowiny@gmail.com>
2025-02-17 14:28:48 +0300

    Implementing JWT Authentication and Validating JWT Authentication in Postman

--- blog-app/blog_app/settings.py
+++ blog-app/blog_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog_api",
     "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "blog_api.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
--- blog-app/blog_app/urls.py
+++ blog-app/blog_app/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog_api.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token-refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
