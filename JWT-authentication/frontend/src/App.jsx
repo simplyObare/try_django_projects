22d9eb1af4e2e89c2b53dd99f962f0a03e4654cd
simplyObare <josephatowiny@gmail.com>
2025-02-26 20:14:43 +0300

    Creating Routes

--- JWT-authentication/frontend/src/App.jsx
+++ JWT-authentication/frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Layout from './pages/Layout'
 
 function App() {
-  return <></>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
--- JWT-authentication/frontend/src/pages/Layout.jsx
+++ JWT-authentication/frontend/src/pages/Layout.jsx
@@ -1,4 +1,22 @@
-const Layout = () => {
-  return <div>Layout</div>
+import { Outlet, Link } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  )
 }
-export default Layout
